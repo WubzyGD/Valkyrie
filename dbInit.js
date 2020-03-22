8145a468eab4129272ef2e80279029e80dc825fa
WubzyGD <wubzygd@gmail.com>
2020-03-22 14:28:15 -0600

    Initialise Bot

--- dbInit.js
+++ dbInit.js
@@ -1,10 +1,19 @@
+function wait(time) {
+	return new Promise(function (resolve) {
+		setTimeout(function () {
+		  	resolve("anything");
+		}, time);
+	});
+};
+
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize('database', 'user', 'password', {
 	host: 'localhost',
 	dialect: 'sqlite',
 	logging: false,
 	storage: 'database.sqlite',
-}).then(async () => {
+})
+wait(100).then(async () => {
     const diceRolled = await sequelize.import("./models/dicerolled");
     const userGameData = await sequelize.import("./models/usergamedata");
 
