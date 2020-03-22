f783d958c3558f3b0199a02f117d2082a69ffd73
WubzyGD <wubzygd@gmail.com>
2020-03-22 14:22:37 -0600

    Initialise Bot

--- dbInit.js
+++ dbInit.js
@@ -4,16 +4,16 @@ const sequelize = new Sequelize('database', 'user', 'password', {
 	dialect: 'sqlite',
 	logging: false,
 	storage: 'database.sqlite',
+}).then(async () => {
+    const diceRolled = await sequelize.import("./models/dicerolled");
+    const userGameData = await sequelize.import("./models/usergamedata");
+
+    var e = process.argv.includes('--force') || process.argv.includes('-f');
+
+    sequelize.sync({force: false}).then(async () => {
+        console.log('Database synced');
+        //sequelize.close();
+    }).catch(console.error);
+
+    module.exports = {diceRolled, userGameData};
 });
-
-const diceRolled = sequelize.import("./models/dicerolled");
-const userGameData = sequelize.import("./models/usergamedata");
-
-var e = process.argv.includes('--force') || process.argv.includes('-f');
-
-sequelize.sync({force: false}).then(async () => {
-	console.log('Database synced');
-	//sequelize.close();
-}).catch(console.error);
-
-module.exports = {diceRolled, userGameData};
