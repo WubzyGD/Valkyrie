cc2c986db361b61d6b77c607970b37d22b3042f9
WubzyGD <WubzyGD@gmail.com>
2020-04-20 20:59:18 -0600

    Initialise Bot

--- bot.js
+++ bot.js
@@ -246,7 +246,7 @@ client.on("message", async message => {
 		} catch (e) {console.log(e); return client.guilds.get("679127746592636949").channels.get("691149517021511722").send("Ope! Looks like nobodu claimed the chest. Whelp Asher, all yours.");};
 	};
 	if (!last_treasureRoll) {var last_treasureRoll = new Date().toString();};
-	console.log((new Date().getTime() - new Date(last_treasureRoll).getTime()) / 1000, last_treasureRoll.getDate(), last_treasureRoll);
+	console.log((new Date().getTime() - new Date(last_treasureRoll).getTime()) / 1000, last_treasureRoll.getTime(), last_treasureRoll);
 	if (((new Date().getTime() - new Date(last_treasureRoll).getTime()) / 1000 >= 60) && Math.floor(Math.random() * 100) <= 2) {spawnTreasure(); var last_treasureRoll = new Date().toString();};
 	if (msg.startsWith(prefix) && (cmd == "dicecount" || cmd == "rollcount")) {
 		var tempDieCount = await diceRolled.findOne({where: {user_id: String(message.author.id)}});
