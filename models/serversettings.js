892092cffd0b9a36be7269e9631bbe70bf8d43a0
WubzyGD <WubzyGD@gmail.com>
2020-04-21 16:07:43 -0600

    Initialise Bot

--- models/serversettings.js
+++ models/serversettings.js
@@ -1,28 +1,28 @@
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('server_settings', {
-        guid_name: Sequelize.STRING,
+        guid_name: DataTypes.STRING,
         guild_id: {
-            type: Sequelize.STRING,
+            type: DataTypes.STRING,
             unique: true
         },
         server_edit_admin_requirement: {
-            type: Sequelize.BOOLEAN,
+            type: DataTypes.BOOLEAN,
             defaultValue: true
         },
         valk_update_channel: {
-            type: Sequelize.STRING,
+            type: DataTypes.STRING,
             defaultValue: "none"
         },
         welcome_message_channel: {
-            type: Sequelize.STRING,
+            type: DataTypes.STRING,
             defaultValue: "none"
         },
         leave_message_channel: {
-            type: Sequelize.STRING,
+            type: DataTypes.STRING,
             defaultValue: "none"
         },
         join_role: {
-            type: Sequelize.STRING,
+            type: DataTypes.STRING,
             defaultValue: "none"
         }
 })};
