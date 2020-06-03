82f027cad9a88cb33e855507d905df624944d12d
WubzyGD <WubzyGD@gmail.com>
2020-06-02 23:54:54 -0600

    Initialise Bot

--- commands/regcmds/pickrandom.js
+++ commands/regcmds/pickrandom.js
@@ -4,7 +4,30 @@ module.exports = {
     name: "pickrandom",
     description: "",
     execute(message, msg, args, cmd, prefix, mention, client) {
-        if (!args.length) {return message.channel.send(`Syntax: \`${prefix}pickrandom <option> <option> <option> <etc...>\``);};
-        return message.reply(new Discord.RichEmbed().setAuthor("Random Item", message.author.avatarURL).setDescription(args[Math.floor(Math.random() * args.length)]).setColor("DC134C"));
+        if (!args.length) {return message.channel.send(`Syntax: \`${prefix}pickrandom $option <stuff> $option <otherStuff> $option <moreStuff> <etc...>\``);};
+        title = "";
+        text = "";
+        tempoption = "";
+        options = [];
+        for (t = 0; t < args.length; t++) {
+			i = args[t];
+			if ((!reading == false) && (!i.startsWith("$"))) {
+				if (reading == "title") {
+					title += `${i} `;
+				} else if (reading == "text") {
+					text += `${i} `;
+				} else {
+					var warn = await message.reply(`\`${i}\` is not a valid option. Use \`${adminPrefix}announce options\` to see a list of valid options.`);
+					warn.delete(10000);
+				};
+			} else if (i.startsWith("$")) {
+				i = i.toLowerCase().slice(1);
+				reading = i;
+				if (reading == "servericonimage") {thumbnail = i;};
+                if (reading == "servericonthumbnail") {thumbnail = message.member.guild.iconURL;};
+                if (reading == "option" && tempoption !== "") {options.push(tempoption); tempoption = "";};
+			};
+        };
+        return message.channel.send(new Discord.RichEmbed().setAuthor("Random Item", message.author.avatarURL).setDescription(args[Math.floor(Math.random() * args.length)]).setColor("DC134C"));
     }
 };
