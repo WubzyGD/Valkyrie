8ffd21e0ada148e79fd857e2abc00450eb47363c
WubzyGD <WubzyGD@gmail.com>
2020-06-30 22:48:27 -0600

    Initialise Bot

--- commands/regcmds/eval.js
+++ commands/regcmds/eval.js
@@ -6,5 +6,15 @@ module.exports = {
     description: "",
     execute(message, msg, args, cmd, prefix, mention, client) {
         if (!message.author.id == "330547934951112705") {return;};
+        try {
+            const code = args.join(" ");
+            let evaled = eval(code);
+       
+            if (typeof evaled !== "string")
+                evaled = require("util").inspect(evaled);
+                message.channel.send(clean(evaled), {code:"xl"});
+            } catch (err) {
+            message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+        }
     }
 };
