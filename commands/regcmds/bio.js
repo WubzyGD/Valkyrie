0ea6e17bb59137abd405d0656cdb400d3d622518
WubzyGD <WubzyGD@gmail.com>
2020-07-03 12:52:07 -0600

    Initialise Bot

--- commands/regcmds/bio.js
+++ commands/regcmds/bio.js
@@ -8,27 +8,29 @@ module.exports = {
         if (!args.length) {return message.channel.send(`Syntax: \`${prefix}bio <set|clear|view>\``);};
         if (args[0] == "set") {
             if (args.length < 2) {return message.reply("You have to provide a bio, silly.");};
-            if (args.shift().join(" ").length > 100) {return message.channel.send("Your bio should be less than 100 characters, please. This is a bio, not a bio*graphy*.");};
+            if (args.join(" ").length > 100) {return message.channel.send("Your bio should be less than 100 characters, please. This is a bio, not a bio*graphy*.");};
+            args.shift();
             if (fs.exists(`./data/bios/${message.author.id}.json`)) {
                 fs.readFile(`./data/bios/${message.author.id}.json`, 'utf8', function readFileCallback(err, data){
                     if (err) {
                         console.log(err);
                     } else {
                     bio = JSON.parse(data);
-                    bio.bio = args.shift().join(" ");
+                    bio.bio = args.join(" ");
                     json = JSON.stringify(bio);
                     fs.writeFile(`./data/bios/${message.author.id}.json`, json, 'utf8', callback);
                     return message.reply("Bio set!");
                 }});
             } else {
                 var bio = {
-                    bio: args.shift().join(" ")
+                    bio: args.join(" ")
                 };
                 var json = JSON.stringify(bio);
                 fs.writeFile(`./data/bios/${message.author.id}.json`, json, 'utf8', callback);
                 return message.reply("Bio set!");
             };
         } else if (args[0] == "clear") {
+            args.shift();
             if (fs.exists(`./data/bios/${message.author.id}.json`)) {
                 fs.readFile(`./data/bios/${message.author.id}.json`, 'utf8', function readFileCallback(err, data){
                     if (err) {
@@ -42,6 +44,7 @@ module.exports = {
                 }});
             } else {return message.reply("You have no bio to clear!")};
         } else if (args[0] == "view") {
+            args.shift();
             if (!mention) {
                 if (fs.exists(`./data/bios/${message.author.id}.json`)) {
                     fs.readFile(`./data/bios/${message.author.id}.json`, 'utf8', function readFileCallback(err, data){
