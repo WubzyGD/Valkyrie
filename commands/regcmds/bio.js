b80fa0a0a016c85f1b5529d742f4a9eb386f21d4
WubzyGD <WubzyGD@gmail.com>
2020-07-03 13:10:28 -0600

    Initialise Bot

--- commands/regcmds/bio.js
+++ commands/regcmds/bio.js
@@ -10,7 +10,7 @@ module.exports = {
             if (args.length < 2) {return message.reply("You have to provide a bio, silly.");};
             if (args.join(" ").length > 100) {return message.channel.send("Your bio should be less than 100 characters, please. This is a bio, not a bio*graphy*.");};
             args.shift();
-            fs.exists(`./data/bios/${message.author.id}.json`, (exists) => {if (exists) {
+            if (fs.existsSync(`./data/bios/${message.author.id}.json`), () => {}) {
                 fs.readFile(`./data/bios/${message.author.id}.json`, 'utf8', function readFileCallback(err, data){
                     if (err) {
                         console.log(err);
@@ -28,10 +28,10 @@ module.exports = {
                 var json = JSON.stringify(bio);
                 fs.writeFile(`./data/bios/${message.author.id}.json`, json, 'utf8', callback);
                 return message.reply("Bio set!");
-            };});
+            };
         } else if (args[0] == "clear") {
             args.shift();
-            fs.exists(`./data/bios/${message.author.id}.json`, (exists) => {if (exists) {
+            if (fs.existsSync(`./data/bios/${message.author.id}.json`)) {
                 fs.readFile(`./data/bios/${message.author.id}.json`, 'utf8', function readFileCallback(err, data){
                     if (err) {
                         console.log(err);
@@ -39,14 +39,14 @@ module.exports = {
                     bio = JSON.parse(data);
                     bio.bio = "Not Set";
                     json = JSON.stringify(bio);
-                    fs.writeFile(`./data/bios/${message.author.id}.json`, json, 'utf8', callback);
+                    fs.writeFileSync(`./data/bios/${message.author.id}.json`, json, 'utf8');
                     return message.reply("Bio cleared!");
                 }});
-            } else {return message.reply("You have no bio to clear!")};});
+            } else {return message.reply("You have no bio to clear!")};
         } else if (args[0] == "view") {
             args.shift();
             if (!mention) {
-                fs.exists(`./data/bios/${message.author.id}.json`, (exists) => {if (exists) {
+                if (fs.existsSync(`./data/bios/${message.author.id}.json`)) {
                     fs.readFile(`./data/bios/${message.author.id}.json`, 'utf8', function readFileCallback(err, data){
                     if (err) {
                         console.log(err);
@@ -54,9 +54,9 @@ module.exports = {
                     bio = JSON.parse(data);
                     return message.reply(new Discord.MessageEmbed().setTitle("User Bio").setThumbnail(message.author.avatarURL()).setDescription(bio.bio).setColor("DC134C"));
                 }});
-                } else {return message.reply("You have no bio for me to show you!")};});
+                } else {return message.reply("You have no bio for me to show you!")};
             } else {
-                fs.exists(`./data/bios/${mention.id}.json`, (exists) => {if (exists) {
+                if (fs.existsSync(`./data/bios/${mention.id}.json`)) {
                     fs.readFile(`./data/bios/${mention.id}.json`, 'utf8', function readFileCallback(err, data){
                     if (err) {
                         console.log(err);
@@ -64,7 +64,7 @@ module.exports = {
                     bio = JSON.parse(data);
                     return message.reply(new Discord.MessageEmbed().setTitle("User Bio").setThumbnail(mention.avatarURL()).setDescription(bio.bio).setColor("DC134C"));
                 }});
-                } else {return message.reply("That user has no bio for me to show you!")};});
+                } else {return message.reply("That user has no bio for me to show you!")};
             };
         }
     }
