86d973926e5b5af2320ff59c01bf53ecb73b34e9
WubzyGD <WubzyGD@gmail.com>
2020-07-06 00:45:51 -0600

    Initialise Bot

--- commands/regcmds/avatar.js
+++ commands/regcmds/avatar.js
@@ -7,7 +7,7 @@ module.exports = {
         if (!args.length) {
             var authorAvatar = new Discord.MessageEmbed()
             .setTitle(`Avatar for ${message.member.displayName}`)
-            .setImage(message.author.avatarURL());
+            .setImage(message.author.avatarURL({size: 2048, dynamic: true}));
             if (message.member.displayName == message.author.username) {
                 message.channel.send(`Avatar for <@${message.author.id}>:`);
                 return message.channel.send(authorAvatar);
@@ -21,7 +21,7 @@ module.exports = {
             };
             var mentionAvatar = new Discord.MessageEmbed()
             .setTitle(`Avatar for ${message.member.guild.members.cache.get(mention.id).displayName}`)
-            .setImage(mention.avatarURL());
+            .setImage(mention.avatarURL({size: 2048, dynamic: true}));
             if (mention.username == message.member.guild.members.cache.get(mention.id).displayName) {
                 message.channel.send(`Avatar for <@${mention.id}>:`);
                 return message.channel.send(mentionAvatar);
