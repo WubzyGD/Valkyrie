43024e99e3e7015a275826d94a529cfdbe2e36cb
WubzyGD <WubzyGD@gmail.com>
2020-07-07 20:49:33 -0600

    Initialise Bot

--- commands/regcmds/avatar.js
+++ commands/regcmds/avatar.js
@@ -8,13 +8,7 @@ module.exports = {
             var authorAvatar = new Discord.MessageEmbed()
             .setTitle(`Avatar for ${message.member.displayName}`)
             .setImage(message.author.avatarURL({size: 2048, dynamic: true}));
-            if (message.member.displayName == message.author.username) {
-                message.channel.send(`Avatar for <@${message.author.id}>:`);
-                return message.channel.send(authorAvatar);
-            } else {
-                message.channel.send(`Avatar for <@!${message.author.id}>:`);
-                return message.channel.send(authorAvatar);
-            };
+            return message.channel.send(authorAvatar);
         } else {
             if (!mention) {
                 return(message.channel.send("You have to mention someone, just to avoid duplicate username confusion :)"));
@@ -22,13 +16,7 @@ module.exports = {
             var mentionAvatar = new Discord.MessageEmbed()
             .setTitle(`Avatar for ${message.member.guild.members.cache.get(mention.id).displayName}`)
             .setImage(mention.avatarURL({size: 2048, dynamic: true}));
-            if (mention.username == message.member.guild.members.cache.get(mention.id).displayName) {
-                message.channel.send(`Avatar for <@${mention.id}>:`);
-                return message.channel.send(mentionAvatar);
-            } else {
-                message.channel.send(`Avatar for <@!${mention.id}>:`);
-                return message.channel.send(mentionAvatar);
-            };
+            return message.channel.send(mentionAvatar);
         };
     }
 };
