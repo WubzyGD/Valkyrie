574825b6bf3a569b1e21af05744b31692c6a923f
WubzyGD <63122940+WubzyGD@users.noreply.github.com>
2020-07-26 15:53:42 -0400

    Initialise Bot

--- commands/regcmds/stats.js
+++ commands/regcmds/stats.js
@@ -80,7 +80,7 @@ module.exports = {
 		if (args[0] == "leaderboard" || args[0] == "l") {
 			if (message.channel.type != "text") {return;};
 			var users = {};
-			message.guild.members.cache.forEach(m => {
+			message.guild.members.cache.forEach(async m => {
 				var ps = await userGameData.findOne({where: {user_id: m.id}});
 				if (ps) {users[m.id] = ps;};
 			});
