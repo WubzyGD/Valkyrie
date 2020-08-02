c717cbe99fb1a054e8f59058ff8cc24bacefe736
WubzyGD <63122940+WubzyGD@users.noreply.github.com>
2020-08-02 18:52:31 -0400

    Initialise Bot

--- bot.js
+++ bot.js
@@ -193,7 +193,7 @@ client.on("ready", async () => {
             'authorization': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0IjoxLCJpZCI6IjYxOTMwNTA2MjkwMDAzOTcyNiIsImlhdCI6MTU5NjQwODU0M30.o7Tfuc_WnRfzjWLyFD2MdjlnbkQ0g77zdowAUb1eb-w"
         },
         body: JSON.stringify({guilds: client.guilds.cache.size, users: client.users.cache.size})
-    }).then(res => res.json());
+    }).then(res => console.log(res.json()));
 } catch (e) {console.log(e)};
 });
 
