b956ed23d7168614a4204dcb887bdada04b9d6ef
WubzyGD <63122940+WubzyGD@users.noreply.github.com>
2020-08-16 20:09:50 -0400

    Bugfix

--- commands/regcmds/guild.js
+++ commands/regcmds/guild.js
@@ -21,6 +21,7 @@ module.exports = {
             if (!client.guilds.cache.get("679127746592636949").members.cache.has(message.author.id)) {return message.reply(`You have to be in the official server to create a guild! Use \`${prefix}supportserver\` to get the invite!`);};
             var pstats = await userGameData.findOne({where: {user_id: message.author.id}});
             if (!pstats || pstats.level < 30 || pstats.money < 30000) {return message.reply("You do not meet the minimum requirements to create a guild! You must be at least level 30 and have 30,000 GP to create a guild.")};
+            if (client.guilds.cache.get().members.has(message.author.id)) {return message.reply(`In order to create a guild, please join the support server with \`${prefix}supportserver\``);};
         };
     }
 };
